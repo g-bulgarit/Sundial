d9c02609629385e13579df4823b74b17c242877d
g-bulgarit <alonio6@gmail.com>
2021-03-05 11:35:09 +0200

    Removed Blynk control folders and files, moved to main

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 #include "ProjectConstants.h"
 #include "SecretKeys.h"
 #include "LedControl.h"
-#include "BlynkControl.h"
 
 // Define an LED array
 CRGB leds[NUM_LEDS];
@@ -32,6 +31,78 @@ void setup() {
   setSyncInterval(5 * 60); // Sync interval in seconds (5 minutes)
 
 }
+
+// Blynk Functions
+BLYNK_WRITE(V1) {
+  TimeInputParam t(param);
+        // Process start time
+        if (t.hasStartTime())
+        {
+            Serial.println(String("Start: ") +
+                        t.getStartHour() + ":" +
+                        t.getStartMinute() + ":" +
+                        t.getStartSecond());
+        }
+        else if (t.isStartSunrise())
+        {
+            Serial.println("Start at sunrise");
+        }
+        else if (t.isStartSunset())
+        {
+            Serial.println("Start at sunset");
+        }
+        else
+        {
+            // Do nothing
+        }
+
+        // Process stop time
+
+        if (t.hasStopTime())
+        {
+            Serial.println(String("Stop: ") +
+                        t.getStopHour() + ":" +
+                        t.getStopMinute() + ":" +
+                        t.getStopSecond());
+        }
+        else if (t.isStopSunrise())
+        {
+            Serial.println("Stop at sunrise");
+        }
+        else if (t.isStopSunset())
+        {
+            Serial.println("Stop at sunset");
+        }
+        else
+        {
+            // Do nothing: no stop time was set
+        }
+
+        // Process timezone
+        // Timezone is already added to start/stop time
+
+        Serial.println(String("Time zone: ") + t.getTZ());
+
+        // Get timezone offset (in seconds)
+        Serial.println(String("Time zone offset: ") + t.getTZ_Offset());
+
+        // Process weekdays (1. Mon, 2. Tue, 3. Wed, ...)
+
+        for (int i = 1; i <= 7; i++) {
+            if (t.isWeekdaySelected(i)) {
+            Serial.println(String("Day ") + i + " is selected");
+            }
+        }
+
+        Serial.println();
+}
+
+BLYNK_WRITE(V2){
+    String currentTime = String(hour()) + ":" + minute() + ":" + second();
+    Serial.print("Time now is:");
+    Serial.println(currentTime);
+}
+
 
 void loop() { 
   Blynk.run();
